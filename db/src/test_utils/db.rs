2d1f5a0c33df14a60bc730e2bb299a08494bd3f4
Alper Gundogdu <mail@alpergundogdu.com>
2025-02-11 15:30:40 +0000

    Rename EnvWrapper to TestEnvWrapper for clarity

--- db/src/test_utils/db.rs
+++ db/src/test_utils/db.rs
@@ -2,23 +2,23 @@ use heed::{Env, EnvOpenOptions};
 use std::fs;
 use std::path::Path;
 
-pub struct EnvWrapper {
+pub struct TestEnvWrapper {
     pub env: Env,
 }
 
-impl Drop for EnvWrapper {
+impl Drop for TestEnvWrapper {
     fn drop(&mut self) {
         fs::remove_dir_all(self.env.path()).unwrap();
     }
 }
 
-pub fn create_test_env() -> EnvWrapper {
+pub fn create_test_env() -> TestEnvWrapper {
     let id = rand::random::<u64>().to_string();
     let path_str = format!("test_db{}", id);
     let path = Path::new(&path_str);
     fs::create_dir(path).unwrap();
     unsafe {
-        EnvWrapper {
+        TestEnvWrapper {
             env: EnvOpenOptions::new()
                 .map_size(20 * 1024 * 1024 * 1024) // 20GB // TODO: set this reasonably!!
                 .max_dbs(3000)
