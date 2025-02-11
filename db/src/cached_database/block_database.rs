37142fd156b7327e5fc042297e5f790b0a3efb83
Alper Gundogdu <mail@alpergundogdu.com>
2025-02-11 14:11:17 +0000

    Don't store unchanged values in the history caches to minimise disk usage
    Remove caches that are too old in reorg, update tests to pass
    
    Refactor docs for clarity and consistency in database module

--- db/src/cached_database/block_database.rs
+++ db/src/cached_database/block_database.rs
@@ -6,14 +6,14 @@ use crate::types::{Decode, Encode, U64ED};
 
 use super::BytesWrapper;
 
-// Database to store data that is mapped to a block number
-//
-// It uses a cache to store the data in memory and only writes to the database when commit is called
-// It also supports reorg by deleting data that is not valid anymore
-// It uses U256 as the key (block_number) and V as the value
-//
-// V: the type of the value to store
-// It should implement Encode, Decode and Clone, so that it can be serialized and deserialized
+/// Database to store data that is mapped to a block number
+///
+/// It uses a cache to store the data in memory and only writes to the database when commit is called
+/// It also supports reorg by deleting data that is not valid anymore
+/// It uses U256 as the key (block_number) and V as the value
+///
+/// V: the type of the value to store
+/// It should implement Encode, Decode and Clone, so that it can be serialized and deserialized
 pub struct BlockDatabase<V>
 where
     V: Encode + Decode + Clone,
@@ -27,15 +27,15 @@ impl<V> BlockDatabase<V>
 where
     V: Encode + Decode + Clone,
 {
-    // Create a new BlockDatabase
-    //
-    // It creates a new database if it does not exist
-    //
-    // env: heed::Env - the environment to store the database
-    // name: &str - the name of the database
-    // parent_wtxn: &mut heed::RwTxn<'_, '_> - the write transaction to use
-    //
-    // Returns: BlockDatabase<V> - the created BlockDatabase
+    /// Create a new BlockDatabase
+    ///
+    /// It creates a new database if it does not exist
+    ///
+    /// env: heed::Env - the environment to store the database
+    /// name: &str - the name of the database
+    /// parent_wtxn: &mut heed::RwTxn<'_, '_> - the write transaction to use
+    ///
+    /// Returns: BlockDatabase<V> - the created BlockDatabase
     pub fn new(env: Env, name: &str, parent_wtxn: &mut RwTxn) -> Self {
         let db: Database<U64ED, BytesWrapper> = {
             let old_db = env
@@ -54,14 +54,14 @@ where
         }
     }
 
-    // Get the value for a block number
-    //
-    // It first checks the cache and then the database
-    // If the value is not found, it returns None
-    // If the value is found, it returns Some(value)
-    //
-    // block_number: u64 - the block number to get the value for
-    // Returns: Option<V> - the value for the block number
+    /// Get the value for a block number
+    ///
+    /// It first checks the cache and then the database
+    /// If the value is not found, it returns None
+    /// If the value is found, it returns Some(value)
+    ///
+    /// block_number: u64 - the block number to get the value for
+    /// Returns: Option<V> - the value for the block number
     pub fn get(&mut self, key: u64) -> Option<V> {
         if let Some(value) = self.cache.get(&key) {
             return Some(value.clone());
@@ -77,22 +77,22 @@ where
         Some(value)
     }
 
-    // Set the value for a block number
-    //
-    // It sets the value in the cache, it's not written to the database until commit is called
-    //
-    // block_number: u64 - the block number to set the value for
-    // value: V - the value to set
+    /// Set the value for a block number
+    ///
+    /// It sets the value in the cache, it's not written to the database until commit is called
+    ///
+    /// block_number: u64 - the block number to set the value for
+    /// value: V - the value to set
     pub fn set(&mut self, block_number: u64, value: V) {
         self.cache.insert(block_number, value.clone());
     }
 
-    // Commit the cache to the database
-    //
-    // It writes all the values in the cache to the database
-    // It does not clear the cache
-    //
-    // parent_wtxn: &mut heed::RwTxn<'_, '_> - the write transaction to use
+    /// Commit the cache to the database
+    ///
+    /// It writes all the values in the cache to the database
+    /// It does not clear the cache
+    ///
+    /// parent_wtxn: &mut heed::RwTxn<'_, '_> - the write transaction to use
     pub fn commit(&mut self, mut parent_wtxn: &mut RwTxn) {
         for (key, value) in self.cache.iter() {
             let value_bytes = BytesWrapper::from_vec(value.encode().unwrap());
@@ -102,22 +102,22 @@ where
         }
     }
 
-    // Clear the cache
-    //
-    // It clears the cache
-    //
-    // This does not delete the data from the database, make sure to call commit before clearing the cache
-    // to write the data to the database, otherwise the data will be lost
+    /// Clear the cache
+    ///
+    /// It clears the cache
+    ///
+    /// This does not delete the data from the database, make sure to call commit before clearing the cache
+    /// to write the data to the database, otherwise the data will be lost
     pub fn clear_cache(&mut self) {
         self.cache.clear();
     }
 
-    // Get the last key in the database
-    //
-    // It returns the last key in the database
-    // If the database is empty, it returns None
-    //
-    // Returns: Option<u64> - the last key in the database
+    /// Get the last key in the database
+    ///
+    /// It returns the last key in the database
+    /// If the database is empty, it returns None
+    ///
+    /// Returns: Option<u64> - the last key in the database
     pub fn last_key(&self) -> Option<u64> {
         let rtxn = self.env.read_txn().unwrap();
         let result = self.db.last(&rtxn).unwrap().map(|(key, _)| key.to_u64());
@@ -134,12 +134,12 @@ where
         }
     }
 
-    // Reorg the database
-    //
-    // It deletes all the data that is not valid anymore, i.e. the data with block number greater than latest_valid_block_number
-    // Make sure to call commit on parent_wtxn after calling this function to write the changes to the database
-    //
-    // parent_wtxn: &mut heed::RwTxn<'_, '_> - the write transaction to use
+    /// Reorg the database
+    ///
+    /// It deletes all the data that is not valid anymore, i.e. the data with block number greater than latest_valid_block_number
+    /// Make sure to call commit on parent_wtxn after calling this function to write the changes to the database
+    ///
+    /// parent_wtxn: &mut heed::RwTxn<'_, '_> - the write transaction to use
     pub fn reorg(&mut self, parent_wtxn: &mut RwTxn, latest_valid_block_number: u64) {
         let mut current = latest_valid_block_number + 1;
         let end = self.last_key().unwrap();
