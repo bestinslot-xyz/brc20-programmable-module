6fcbe4eb2239f5618df4645c46c2fef0755fbc87
Alper Gundogdu <mail@alpergundogdu.com>
2025-03-14 12:13:47 +0000

    Refactor precompiles: rename and reorganize modules for clarity and add empty GetLockedPkScript precompile

--- server/src/evm/precompiles/mod.rs
+++ server/src/evm/precompiles/mod.rs
@@ -1,15 +1,18 @@
 
-mod bip322_precompile;
-pub use bip322_precompile::*;
+mod bip322_verify_precompile;
+pub use bip322_verify_precompile::*;
 
-mod btc_precompile;
-pub use btc_precompile::*;
+mod btc_tx_details_precompile;
+pub use btc_tx_details_precompile::*;
 
-mod brc20_precompile;
-pub use brc20_precompile::*;
+mod brc20_balance_precompile;
+pub use brc20_balance_precompile::*;
 
-mod last_sat_loc_precompile;
-pub use last_sat_loc_precompile::*;
+mod btc_last_sat_loc_precompile;
+pub use btc_last_sat_loc_precompile::*;
+
+mod get_locked_pkscript_precompile;
+pub use get_locked_pkscript_precompile::*;
 
 mod precompiles;
 pub use precompiles::load_precompiles;
