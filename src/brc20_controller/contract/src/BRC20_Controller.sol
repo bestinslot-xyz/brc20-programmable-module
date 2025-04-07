02ac74349dd4e4584c0130d4b04f1b7816ed9b6c
Halil Samed Çıldır <samedcildir@gmail.com>
2025-04-07 15:45:15 +0300

    Updated BRC20_Controller.sol to deploy Wrapped BRC-20 on new ticker mints.

--- src/brc20_controller/contract/src/BRC20_Controller.sol
+++ src/brc20_controller/contract/src/BRC20_Controller.sol
@@ -5,15 +5,169 @@ pragma solidity ^0.8.19;
 import {IBRC20_Controller} from "./IBRC20_Controller.sol";
 import {Context} from "./utils/Context.sol";
 import {Ownable} from "./access/Ownable.sol";
-import {IBRC20_ControllerErrors} from "./interfaces/draft-IBRC6093.sol";
+import {IERC20Errors} from "./interfaces/draft-IERC6093.sol";
+import {IERC20Metadata} from "./interfaces/IERC20Metadata.sol";
+
+contract BRC20 is Ownable, IERC20Metadata, IERC20Errors {
+    mapping(address account => uint256) private _balances;
+    mapping(address account => mapping(address spender => uint256)) private _allowances;
+
+    uint256 private _totalSupply;
+
+    string private _name;
+    string private _symbol;
+
+    /**
+     * @dev Sets the values for {name} and {symbol}.
+     *
+     * Both values are immutable: they can only be set once during construction.
+     */
+    constructor(string memory name_, string memory symbol_) Ownable(_msgSender()) {
+        _name = name_;
+        _symbol = symbol_;
+    }
+
+    function name() public view virtual returns (string memory) {
+        return _name;
+    }
+    function symbol() public view virtual returns (string memory) {
+        return _symbol;
+    }
+    function decimals() public view virtual returns (uint8) {
+        return 18;
+    }
+    function totalSupply() public view virtual returns (uint256) {
+        return _totalSupply;
+    }
+    function balanceOf(address account) public view virtual returns (uint256) {
+        return _balances[account];
+    }
+    function transfer(address to, uint256 value) public virtual returns (bool) {
+        address owner = _msgSender();
+        _transfer(owner, to, value);
+        return true;
+    }
+    function allowance(address owner, address spender) public view virtual returns (uint256) {
+        if (spender == owner) {
+            return type(uint256).max;
+        }
+        return _allowances[owner][spender];
+    }
+    function approve(address spender, uint256 value) public virtual returns (bool) {
+        address owner = _msgSender();
+        _approve(owner, spender, value);
+        return true;
+    }
+    function transferFrom(address from, address to, uint256 value) public virtual returns (bool) {
+        address spender = _msgSender();
+        _spendAllowance(from, spender, value);
+        _transfer(from, to, value);
+        return true;
+    }
+
+
+    function approve(address owner, address spender, uint256 value) public onlyOwner returns (bool) {
+        _approve(owner, spender, value);
+        return true;
+    }
+    function transferFrom(address spender, address from, address to, uint256 value) public onlyOwner returns (bool) {
+        _spendAllowance(from, spender, value);
+        _transfer(from, to, value);
+        return true;
+    }
+    function mint(address account, uint256 value) public onlyOwner returns (bool) {
+        _mint(account, value);
+        return true;
+    }
+    function burn(address account, uint256 value) public onlyOwner returns (bool) {
+        _burn(account, value);
+        return true;
+    }
+
+
+    function _transfer(address from, address to, uint256 value) internal {
+        if (from == address(0)) {
+            revert ERC20InvalidSender(address(0));
+        }
+        if (to == address(0)) {
+            revert ERC20InvalidReceiver(address(0));
+        }
+        _update(from, to, value);
+    }
+    function _update(address from, address to, uint256 value) internal virtual {
+        if (from == address(0)) {
+            // Overflow check required: The rest of the code assumes that totalSupply never overflows
+            _totalSupply += value;
+        } else {
+            uint256 fromBalance = _balances[from];
+            if (fromBalance < value) {
+                revert ERC20InsufficientBalance(from, fromBalance, value);
+            }
+            unchecked {
+                // Overflow not possible: value <= fromBalance <= totalSupply.
+                _balances[from] = fromBalance - value;
+            }
+        }
+
+        if (to == address(0)) {
+            unchecked {
+                // Overflow not possible: value <= totalSupply or value <= fromBalance <= totalSupply.
+                _totalSupply -= value;
+            }
+        } else {
+            unchecked {
+                // Overflow not possible: balance + value is at most totalSupply, which we know fits into a uint256.
+                _balances[to] += value;
+            }
+        }
+
+        emit Transfer(from, to, value);
+    }
+    function _mint(address account, uint256 value) internal {
+        if (account == address(0)) {
+            revert ERC20InvalidReceiver(address(0));
+        }
+        _update(address(0), account, value);
+    }
+    function _burn(address account, uint256 value) internal {
+        if (account == address(0)) {
+            revert ERC20InvalidSender(address(0));
+        }
+        _update(account, address(0), value);
+    }
+    function _approve(address owner, address spender, uint256 value) internal {
+        _approve(owner, spender, value, true);
+    }
+    function _approve(address owner, address spender, uint256 value, bool emitEvent) internal virtual {
+        if (owner == address(0)) {
+            revert ERC20InvalidApprover(address(0));
+        }
+        if (spender == address(0)) {
+            revert ERC20InvalidSpender(address(0));
+        }
+        _allowances[owner][spender] = value;
+        if (emitEvent) {
+            emit Approval(owner, spender, value);
+        }
+    }
+    function _spendAllowance(address owner, address spender, uint256 value) internal virtual {
+        uint256 currentAllowance = allowance(owner, spender);
+        if (currentAllowance < type(uint256).max) {
+            if (currentAllowance < value) {
+                revert ERC20InsufficientAllowance(spender, currentAllowance, value);
+            }
+            unchecked {
+                _approve(owner, spender, currentAllowance - value, false);
+            }
+        }
+    }
+}
 
 /**
  * @dev Implementation of the {IBRC20_Controller} interface.
  */
-contract BRC20_Controller is Context, Ownable, IBRC20_Controller, IBRC20_ControllerErrors {
-    mapping(bytes => mapping(address account => uint256)) private _balances;
-
-    mapping(bytes => mapping(address account => mapping(address spender => uint256))) private _allowances;
+contract BRC20_Controller is Context, Ownable, IBRC20_Controller {
+    mapping(bytes => BRC20) private _brc20s;
 
     /**
      * @dev Sets the values for {name} and {symbol}.
@@ -24,11 +178,15 @@ contract BRC20_Controller is Context, Ownable, IBRC20_Controller, IBRC20_Control
     constructor() Ownable(_msgSender()) {
     }
 
+    function getTickerAddress(bytes calldata ticker) public view returns (address) {
+        return address(_brc20s[ticker]);
+    }
+
     /**
      * @dev See {IBRC20_Controller-balanceOf}.
      */
     function balanceOf(bytes calldata ticker, address account) public view virtual returns (uint256) {
-        return _balances[ticker][account];
+        return _brc20s[ticker].balanceOf(account);
     }
 
     /**
@@ -41,15 +199,15 @@ contract BRC20_Controller is Context, Ownable, IBRC20_Controller, IBRC20_Control
      */
     function transfer(bytes calldata ticker, address to, uint256 value) public virtual returns (bool) {
         address owner = _msgSender();
-        _transfer(ticker, owner, to, value);
-        return true;
+        emit Transfer(ticker, owner, to, value);
+        return _brc20s[ticker].transferFrom(owner, to, value);
     }
 
     /**
      * @dev See {IBRC20_Controller-allowance}.
      */
     function allowance(bytes calldata ticker, address owner, address spender) public view virtual returns (uint256) {
-        return _allowances[ticker][owner][spender];
+        return _brc20s[ticker].allowance(owner, spender);
     }
 
     /**
@@ -64,8 +222,8 @@ contract BRC20_Controller is Context, Ownable, IBRC20_Controller, IBRC20_Control
      */
     function approve(bytes calldata ticker, address spender, uint256 value) public virtual returns (bool) {
         address owner = _msgSender();
-        _approve(ticker, owner, spender, value);
-        return true;
+        emit Approval(ticker, owner, spender, value);
+        return _brc20s[ticker].approve(owner, spender, value);
     }
 
     /**
@@ -86,174 +244,22 @@ contract BRC20_Controller is Context, Ownable, IBRC20_Controller, IBRC20_Control
      */
     function transferFrom(bytes calldata ticker, address from, address to, uint256 value) public virtual returns (bool) {
         address spender = _msgSender();
-        _spendAllowance(ticker, from, spender, value);
-        _transfer(ticker, from, to, value);
-        return true;
+        emit Transfer(ticker, from, to, value);
+        return _brc20s[ticker].transferFrom(spender, from, to, value);
     }
 
     function mint(bytes calldata ticker, address to, uint256 value) onlyOwner public virtual returns (bool) {
-        _mint(ticker, to, value);
-        return true;
+        if (address(_brc20s[ticker]) == address(0)) {
+            _brc20s[ticker] = new BRC20(string(ticker), string(ticker));
+            emit BRC20Created(ticker, address(_brc20s[ticker]));
+        }
+        
+        emit Transfer(ticker, address(0), to, value);
+        return _brc20s[ticker].mint(to, value);
     }
 
     function burn(bytes calldata ticker, address from, uint256 value) onlyOwner public virtual returns (bool) {
-        _burn(ticker, from, value);
-        return true;
-    }
-
-    /**
-     * @dev Moves a `value` amount of tokens from `from` to `to`.
-     *
-     * This internal function is equivalent to {transfer}, and can be used to
-     * e.g. implement automatic token fees, slashing mechanisms, etc.
-     *
-     * Emits a {Transfer} event.
-     *
-     * NOTE: This function is not virtual, {_update} should be overridden instead.
-     */
-    function _transfer(bytes calldata ticker, address from, address to, uint256 value) internal {
-        if (from == address(0)) {
-            revert BRC20InvalidSender(ticker, address(0));
-        }
-        if (to == address(0)) {
-            revert BRC20InvalidReceiver(ticker, address(0));
-        }
-        _update(ticker, from, to, value);
-    }
-
-    /**
-     * @dev Transfers a `value` amount of tokens from `from` to `to`, or alternatively mints (or burns) if `from`
-     * (or `to`) is the zero address. All customizations to transfers, mints, and burns should be done by overriding
-     * this function.
-     *
-     * Emits a {Transfer} event.
-     */
-    function _update(bytes calldata ticker, address from, address to, uint256 value) internal virtual {
-        if (from == address(0)) {
-            // Overflow check required: The rest of the code assumes that totalSupply never overflows
-            // _totalSupply += value;
-        } else {
-            uint256 fromBalance = _balances[ticker][from];
-            if (fromBalance < value) {
-                revert BRC20InsufficientBalance(ticker, from, fromBalance, value);
-            }
-            unchecked {
-                // Overflow not possible: value <= fromBalance <= totalSupply.
-                _balances[ticker][from] = fromBalance - value;
-            }
-        }
-
-        if (to == address(0)) {
-            unchecked {
-                // Overflow not possible: value <= totalSupply or value <= fromBalance <= totalSupply.
-                // _totalSupply -= value;
-            }
-        } else {
-            unchecked {
-                // Overflow not possible: balance + value is at most totalSupply, which we know fits into a uint256.
-                _balances[ticker][to] += value;
-            }
-        }
-
-        emit Transfer(ticker, from, to, value);
-    }
-
-    /**
-     * @dev Creates a `value` amount of tokens and assigns them to `account`, by transferring it from address(0).
-     * Relies on the `_update` mechanism
-     *
-     * Emits a {Transfer} event with `from` set to the zero address.
-     *
-     * NOTE: This function is not virtual, {_update} should be overridden instead.
-     */
-    function _mint(bytes calldata ticker, address account, uint256 value) internal {
-        if (account == address(0)) {
-            revert BRC20InvalidReceiver(ticker, address(0));
-        }
-        _update(ticker, address(0), account, value);
-    }
-
-    /**
-     * @dev Destroys a `value` amount of tokens from `account`, lowering the total supply.
-     * Relies on the `_update` mechanism.
-     *
-     * Emits a {Transfer} event with `to` set to the zero address.
-     *
-     * NOTE: This function is not virtual, {_update} should be overridden instead
-     */
-    function _burn(bytes calldata ticker, address account, uint256 value) internal {
-        if (account == address(0)) {
-            revert BRC20InvalidSender(ticker, address(0));
-        }
-        _update(ticker, account, address(0), value);
-    }
-
-    /**
-     * @dev Sets `value` as the allowance of `spender` over the `owner` s tokens.
-     *
-     * This internal function is equivalent to `approve`, and can be used to
-     * e.g. set automatic allowances for certain subsystems, etc.
-     *
-     * Emits an {Approval} event.
-     *
-     * Requirements:
-     *
-     * - `owner` cannot be the zero address.
-     * - `spender` cannot be the zero address.
-     *
-     * Overrides to this logic should be done to the variant with an additional `bool emitEvent` argument.
-     */
-    function _approve(bytes calldata ticker, address owner, address spender, uint256 value) internal {
-        _approve(ticker, owner, spender, value, true);
-    }
-
-    /**
-     * @dev Variant of {_approve} with an optional flag to enable or disable the {Approval} event.
-     *
-     * By default (when calling {_approve}) the flag is set to true. On the other hand, approval changes made by
-     * `_spendAllowance` during the `transferFrom` operation set the flag to false. This saves gas by not emitting any
-     * `Approval` event during `transferFrom` operations.
-     *
-     * Anyone who wishes to continue emitting `Approval` events on the`transferFrom` operation can force the flag to
-     * true using the following override:
-     * ```
-     * function _approve(address owner, address spender, uint256 value, bool) internal virtual override {
-     *     super._approve(owner, spender, value, true);
-     * }
-     * ```
-     *
-     * Requirements are the same as {_approve}.
-     */
-    function _approve(bytes calldata ticker, address owner, address spender, uint256 value, bool emitEvent) internal virtual {
-        if (owner == address(0)) {
-            revert BRC20InvalidApprover(ticker, address(0));
-        }
-        if (spender == address(0)) {
-            revert BRC20InvalidSpender(ticker, address(0));
-        }
-        _allowances[ticker][owner][spender] = value;
-        if (emitEvent) {
-            emit Approval(ticker, owner, spender, value);
-        }
-    }
-
-    /**
-     * @dev Updates `owner` s allowance for `spender` based on spent `value`.
-     *
-     * Does not update the allowance value in case of infinite allowance.
-     * Revert if not enough allowance is available.
-     *
-     * Does not emit an {Approval} event.
-     */
-    function _spendAllowance(bytes calldata ticker, address owner, address spender, uint256 value) internal virtual {
-        uint256 currentAllowance = allowance(ticker, owner, spender);
-        if (currentAllowance != type(uint256).max) {
-            if (currentAllowance < value) {
-                revert BRC20InsufficientAllowance(ticker, spender, currentAllowance, value);
-            }
-            unchecked {
-                _approve(ticker, owner, spender, currentAllowance - value, false);
-            }
-        }
+        emit Transfer(ticker, from, address(0), value);
+        return _brc20s[ticker].burn(from, value);
     }
 }
