5df80c229fd343b1dc519c975f62dce487dccc7b
Alper Gundogdu <mail@alpergundogdu.com>
2025-12-16 12:58:42 +0000

    feat: Change tx hash computation to use RLP hash from a set block height

--- src/engine/hardforks.rs
+++ src/engine/hardforks.rs
@@ -27,3 +27,15 @@ pub fn get_evm_spec(block_number: u64) -> SpecId {
         _ => SpecId::PRAGUE,
     }
 }
+
+const RLP_HASH_ACTIVATION_HEIGHT_MAINNET: u64 = 929_000; // 22 Dec 2025, reindexing not required
+const RLP_HASH_ACTIVATION_HEIGHT_SIGNET: u64 = 0; // Always use RLP hash on Signet, reindexing required
+
+pub fn use_rlp_hash_for_tx_hash(block_number: u64) -> bool {
+    let network = get_bitcoin_network();
+    match network {
+        Network::Bitcoin => block_number >= RLP_HASH_ACTIVATION_HEIGHT_MAINNET,
+        Network::Signet => block_number >= RLP_HASH_ACTIVATION_HEIGHT_SIGNET,
+        _ => true,
+    }
+}
